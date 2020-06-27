e29bb5e3cd31e4c3c5b1709ff484f3812803222f
Darren <dartrenbbx@gmail.com>
2020-06-26 22:15:26 -0700

    migrations
    gitignore and setup stuff

--- react-django/practice/leadmanager/leadmanager/urls.py
+++ react-django/practice/leadmanager/leadmanager/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    # load frontend first
+    path('', include('frontend.urls')),
     path('', include('leads.urls')),
 ]
